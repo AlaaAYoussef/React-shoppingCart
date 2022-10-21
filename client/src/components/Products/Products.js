44e8c1f24be66f40853797db341fce5aa6e251c6
AlaaAYoussef <alaaabbasyoussef@gmail.com>
2022-10-21 21:52:54 +0200

    products-update

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,20 +1,19 @@
 import React from "react";
+import '../../css/Products/Products.css'
 
 function Products(props) {
   return (
     <div className="products-wrapper">
-      {props.products.map((product) => {
-        return (
-          <div className="product-item" key={product.id}>
-            <img src={product.imgURL} alt={product.title}/>
-            {/* {console.log(product.imgUrl)} */}
-            <div className="product-desc">
-              <p>{product.title}</p>
-              <span>{product.price}</span>
-            </div>
+      {props.products.map((product) => (
+        <div className="product-item" key={product.id}>
+          <img src={product.imgURL} alt={product.title} />
+          <div className="product-desc">
+            <p>{product.title}</p>
+            <span>{product.price}</span>
           </div>
-        );
-      })}
+          <button>Add To Cart</button>
+        </div>
+      ))}
     </div>
   );
 }
