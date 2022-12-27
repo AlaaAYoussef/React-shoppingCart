86e906b6b8188b528f7e5beb8b674a39f1795a20
AlaaAYoussef <alaaabbasyoussef@gmail.com>
2022-12-27 23:28:14 +0200

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,33 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import Products from "./components/Products/Products";
-import Filter from "./components/Filter/Filter";
 import Cart from "./components/Cart/Cart";
 import { Provider } from "react-redux";
 import store from './store/store'
+import Home from "./pages/Home";
+import Orders from './pages/Order'
+import Navbar from "./components/Navbar/Navbar";
+import {BrowserRouter,Routes,Route,NavLink} from 'react-router-dom'
 
 
 function App() {
  
 
   return (
+    <BrowserRouter>
     <Provider store={store}>
     <div className="layout">
       <Header />
       <main>
-        <div className="wrapper">
-          <Products products={products} addToCart={addToCart}/>
-          <Filter/>
-        </div>
-        <Cart/>
-     
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>} exact></Route>
+          <Route path="/orders" element={<Orders/>} exact></Route>
+        </Routes>
       </main>
       <Footer />
     </div>
     </Provider>
+    </BrowserRouter>
   );
 }
 
